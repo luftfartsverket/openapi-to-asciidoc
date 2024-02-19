3402e4a085aa4e1fab4a01430d16030b88d841c0
Jimisola Laursen <85176191+lfvjimisola@users.noreply.github.com>
2024-02-19 13:01:22 +0100

    fix: Use __package__ to determine execution start (#13)

--- src/openapi_to_asciidoc/convert.py
+++ src/openapi_to_asciidoc/convert.py
@@ -5,20 +5,50 @@
 
 import argparse
 import json
+import os
+import sys
 from importlib.metadata import version
+from typing import TextIO, Union
 
-from openapi_to_asciidoc.objects import OpenApi, OpenApiSchema
+if __package__ is None:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    from openapi_to_asciidoc.objects import OpenApi, OpenApiSchema
+else:
+    from openapi_to_asciidoc.objects import OpenApi, OpenApiSchema
+
+
+@staticmethod
+def create_directory_and_open(file_path: Union[TextIO, str]) -> TextIO:
+    """
+    Create the directory if it doesn't exist and open the specified file.
+
+    Parameters:
+    - file_path (TextIO (sys.stdout) or str (path as argument on command line): The file path.
+    - mode (str): The mode in which the file should be opened.
+
+    Returns:
+    - TextIO: The opened file.
+
+    If the file path is sys.stdout, it is returned as is without attempting to create the directory.
+    """
+    if file_path == sys.stdout:
+        return file_path
+
+    directory = os.path.dirname(os.path.abspath(file_path))
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return open(file_path, "w")
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version=version("maven-artifact"),
-    )
+    ver: str = "local dev" if __package__ is None else f"{version('openapi-to-asciidoc')}"
+
+    parser.add_argument("-V", "--version", action="version", version=ver)
+
     parser.add_argument(
         "-j",
         "--json",
@@ -30,9 +60,9 @@ def get_arguments():
         "-o",
         "--output",
         nargs="?",
-        help="Where to output result (default: openapi.adoc)",
-        type=argparse.FileType("w"),
-        default="openapi.adoc",
+        help="Where to output result (default: stdout)",
+        type=lambda file: create_directory_and_open(file),
+        default=sys.stdout,
     )
 
     return parser.parse_args()
